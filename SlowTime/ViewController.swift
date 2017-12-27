c5ba42ed5bbeb5f4aec06aaa993bc7d66fa2d6a6
pingchang.ke <kpc@vincross.com>
2017-12-27 19:14:13 +0800

    new project.

---
+++ SlowTime/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SlowTime
+//
+//  Created by KKING on 2017/12/27.
+//  Copyright © 2017年 KKING. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
